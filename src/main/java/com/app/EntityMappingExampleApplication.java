457dfa295a83d9c083b43e0fce4bc6c708b3fdc5
YashPardhi14 <yapardhi@gmail.com>
2024-01-26 12:24:54 +0530

    First commit

---
+++ src/main/java/com/app/EntityMappingExampleApplication.java
@@ -0,0 +1,13 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntityMappingExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntityMappingExampleApplication.class, args);
+	}
+
+}
